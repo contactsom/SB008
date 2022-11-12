820572c35806c8607d8beb98b2613507b6b8758b
om <apipothi.com>
2022-11-12 23:19:13 +0530

    Core JAVA Day -1

---
+++ Day1/src/Hello.java
@@ -0,0 +1,19 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello Wolrd");
+	}
+
+}
+//  1. JAVA Installed 
+//  2. Eclipse Installed
+//  3. We are master in JAVA now :)
+
+// I develop Hello.java using - JDK - Agree
+// Run this file Hello.java in your system
+	// What "MINIMUM" you need to run this file JDK or JRE ? JRE - OK 
+	// I am giving you JDK - What Wrong I am doing : NO
+
+//  
